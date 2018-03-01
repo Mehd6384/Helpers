607cc05a66a5dbcb5fa822f67d7f0302a564da4e
Mehdi <mounsif.mehdi@gmail.com>
2018-03-01 16:43:11 +0100

    Add files via upload

---
+++ Curriculum.py
@@ -0,0 +1,64 @@
+import numpy as np 
+
+# To use this class, first generate levels and pass them to the Planner. Start the Planner 
+# and have it check performance of the agent regurlary 
+
+#Example at the end of the script
+class Level(): 
+
+	def __init__(self, success_min, positions,name = '0'): 
+
+		self.success_min = success_min
+		self.positions = positions
+		self.name = name
+
+	def create_next_level(self, success_min, additional_pos, name = 'next'): 
+
+		new = Level(success_min, self.positions[:], name)
+		for p in additional_pos: 
+			new.positions.append(p)
+		return new 
+
+class Planner(): 
+
+	def __init__(self, env, planning):
+
+		self.level_list = planning
+		# for p in self.level_list: 
+		# 	print('{} -- Positions {}\n'.format(p.name, p.positions))
+
+		self.env = env
+		self.level = 0 
+
+	def get_architecture(self): 
+
+		for p in self.level_list: 
+			print('{} -- Positions {}\n'.format(p.name, p.positions))
+
+	def start(self): 
+
+		print('\n***** Starting curriculum *****\n')
+		self.env.setTargetPosition(self.level_list[0].positions)
+		# for p in self.level_list[0].positions: 
+		# 	print(p)
+		# input()
+
+	def adjust_complexity(self, successes): 
+
+		current = self.level_list[self.level]
+		if successes > current.success_min: 
+			self.level = min(self.level+1, len(self.level_list)-1)
+			print('Upgrading -- New level is : ', self.level)
+			self.env.setTargetPosition(self.level_list[self.level].positions) 
+			# for p in self.level_list[self.level].positions: 
+			# 	print(p)
+
+# level_00 = Level(80, [[0.3,0.6], [0.7,0.6]], 'Absolute beginner')
+# level_0 = level_00.create_next_level(80, [[0.1,0.6], [0.9,0.6]], 'Beginner')
+# level_1 = level_0.create_next_level(80, [[0.1,0.3], [0.9,0.3]], 'Easy')
+# level_2 = level_1.create_next_level(80, [[0.1,0.1], [0.9,0.1]], 'Medium')
+# level_3 = level_2.create_next_level(100, [[0.5,0.5], [0.3,0.3], [0.6,0.3]],'Hard')
+
+# planning = Planner(env, [level_00, level_0, level_1, level_2, level_3])
+
+# planning.start()
--- FastPlot.py
+++ FastPlot.py
@@ -3,7 +3,8 @@ import numpy as np
 
 
 # To use this class: Simply initialize with the number of actions you want to monitor 
-# Then, call draw whenever you want to update the plot 
+# Then, call draw whenever you want to update the plot
+# It is possible to set up names for the bars: After creating an instance, call instance.set_names(names) 
 
 class ThinkFast(): 
 
@@ -38,7 +39,6 @@ class ThinkFast():
 		if self.has_name: 
 			self.ax.set_xticks(np.arange(self.nb_actions)*1. + 0.5)
 			self.ax.set_xticklabels(self.names)
-			print('set')
 		self.ax.set_ylim(0.,1.)
 
 		x = np.arange(self.nb_actions)
@@ -51,6 +51,90 @@ class ThinkFast():
 		for v,b in zip(vals,self.bars): 
 			b.set_height(v)
 			self.ax.draw_artist(b)
+
+		self.fig.canvas.update()
+		self.fig.canvas.flush_events()
+
+
+
+class FastStateValue(): # To use this class, initialize with max steps of the environment. Then, draw with the latest state 
+						# value and information concerning termination of the episode 
+						# Also, using previous allows to see previous state estimations of the critic. Can be useful
+
+	def __init__(self, max_length, previous = 3): 
+
+		self.max_length = max_length + 1
+		self.values = np.zeros((self.max_length))
+		self.count = 0
+		self.up = False
+		self.show_old = False
+		if previous > 0: 
+			self.previous_size = previous
+			self.show_old = True
+			self.count_old = 0
+			self.previous = np.zeros((previous, self.max_length))
+
+	def draw(self,y,done):
+
+		self.values[self.count] = y
+		self.count += 1 
+
+		if done: 
+			self.count = 0 
+			
+			if self.show_old: 
+				for i in reversed(range(self.previous_size)): 
+					if i == 0: 
+						self.previous[i,:] = self.values
+					else: 
+						self.previous[i,:] = self.previous[i-1,:]
+				for i in range(self.previous_size): 
+					self.old_lines[i].set_ydata(self.previous[i,:])
+				# self.previous[self.count_old,:] = self.values
+				# self.count_old = (self.count_old+1)%self.previous_size
+				# for i in range(self.previous_size): 
+				# 	self.old_lines[i].set_ydata(self.previous[i,:])
+
+			self.values *= 0. 
+
+		if not self.up: 
+			self.up = True
+			self.init_plot()
+
+		self.update(done)
+
+	def init_plot(self): 
+
+		plt.style.use('ggplot')
+		plt.ion()
+
+		self.fig, self.ax = plt.subplots()
+		self.ax.set_title('States values in current episode')
+		self.ax.set_xlabel('Steps in episode')
+		self.ax.set_ylabel('State value')
+		self.ax.set_xlim(0,self.max_length)
+
+		self.line, = self.ax.plot(np.arange(self.max_length), self.values)
+
+		if self.show_old: 
+			self.old_lines = []
+			for i in range(self.previous_size): 
+				l, = self.ax.plot(np.arange(self.max_length), self.values)
+				self.old_lines.append(l)
+
+			legende = ['State values at n-{}'.format(i) for i in range(self.previous_size)]
+			legende.insert(0,'Current state values')
+			self.ax.legend(legende)
+
+		self.fig.canvas.draw()
+
+	def update(self, done): 
+
+		self.line.set_ydata(self.values)
+		self.ax.draw_artist(self.line)
+		v_min, v_max = np.min(self.values), np.max(self.values)
+		big = np.max([np.abs(v_min), v_max])
+		if not done: self.ax.set_ylim(-big*1.1, big*1.1)
 
 		self.fig.canvas.update()
 		self.fig.canvas.flush_events()
