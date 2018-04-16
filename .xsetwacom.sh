34ad1ad60958a65748d1cac1c42a9906eec2f254
Mehdi <mounsif.mehdi@gmail.com>
2018-04-16 13:19:09 +0200

    Wacom configuration file

---
+++ .xsetwacom.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+
+xsetwacom set "Wacom Intuos PT S 2 Pen stylus" mode Relative
+
+
+
+xsetwacom set "Wacom Intuos PT S 2 Pad pad" Button 1 "key ctrl z"
+xsetwacom set "Wacom Intuos PT S 2 Pad pad" Button 3 "key f"
+xsetwacom set "Wacom Intuos PT S 2 Pad pad" Button 8 "key ctrl +"
+xsetwacom set "Wacom Intuos PT S 2 Pad pad" Button 9 "KEY ctrl -"
